599af30c5cb31afd07056e24b3173637e654662b
dashlw <daisha.walton@du.edu>
2016-12-08 16:42:48 -0700

    debuggin

--- knexfile.js
+++ knexfile.js
@@ -6,11 +6,11 @@ module.exports = {
 
     development: {
         client: 'pg',
-        connection: process.env.DATABASE_URL
+        connection: process.env.DEV_DATABASE_URL
     },
 
     production: {
-        client: 'postgresql',
+        client: 'pg',
         connection: process.env.DATABASE_URL
     }
 
